b6aebf4a408da0207db52706443435d0bd562469
Matthew <aladornxd@gmail.com>
2016-03-08 18:23:36 -0300

    Update index.php

--- aulaxcodigohipotenusa/index.php
+++ aulaxcodigohipotenusa/index.php
@@ -8,6 +8,8 @@
 
 <form action="Controller.php" method="post">
 <input name="position" type="text"/> 
+  <input name="catetoAdj" type="text"/>
+	<input name="catetoOp" type="text"/>
 <input type="submit" value="Hipotenusa"/>
 
 
