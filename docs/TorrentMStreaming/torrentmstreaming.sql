7ba2c82b449667516a266dbd944bb13e2030ba22
Matthew <aladornxd@gmail.com>
2016-05-05 16:27:56 -0300

    banco

---
+++ docs/TorrentMStreaming/torrentmstreaming.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 05-Maio-2016 às 21:26
+-- Versão do servidor: 10.1.10-MariaDB
+-- PHP Version: 5.6.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `torrentmstreaming`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `uploadtorrent`
+--
+
+CREATE TABLE `uploadtorrent` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `gen` varchar(50) NOT NULL,
+  `quality` varchar(50) NOT NULL,
+  `sound_quality` text NOT NULL,
+  `resolution` text NOT NULL,
+  `upload` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `uploadtorrent`
+--
+
+INSERT INTO `uploadtorrent` (`id`, `first_name`, `gen`, `quality`, `sound_quality`, `resolution`, `upload`) VALUES
+(1, 'flash', 'terror', 'fullhd', 'dts5.1', '1920x1080', 'seriadoHD');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `birthdate` date NOT NULL,
+  `phone` varchar(25) NOT NULL,
+  `pass` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `user`
+--
+
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `birthdate`, `phone`, `pass`) VALUES
+(1, 'Mateus', 'Mourao', 'aladornxd@gmail.com', '1991-09-20', '81675250', 'restDOcAo123'),
+(3, 'ateus', 'Mourao', 'aladornxd@gmail.com', '1991-09-20', '81675250', 'restDOcAo123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `uploadtorrent`
+--
+ALTER TABLE `uploadtorrent`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `uploadtorrent`
+--
+ALTER TABLE `uploadtorrent`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
