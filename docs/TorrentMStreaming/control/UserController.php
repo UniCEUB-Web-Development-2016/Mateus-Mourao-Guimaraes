a539434b4dda11d336028a924e181439b012bd80
Matthew <aladornxd@gmail.com>
2016-06-21 15:12:05 -0300

    Add files via upload

--- docs/TorrentMStreaming/control/UserController.php
+++ docs/TorrentMStreaming/control/UserController.php
@@ -5,33 +5,38 @@ include_once "database/DatabaseConnector.php";
 
 class UserController
 {
+private $requiredParameters = array('first_name', 'last_name', 'email', 'birthdate', 'phone', 'password');
 	public function register($request)
 	{
 		$params = $request->get_params();
+		if($this->isValid($params)){
 		$user = new User($params["first_name"],
-			$params["last_name"],
-			$params["email"],
-			$params["birthdate"],
-			$params["phone"],
-			$params["password"]);
-		$db = new DatabaseConnector("localhost", "TorrentMStreaming", "mysql", "", "root", "");
-		$conn = $db->getConnection();
+				 $params["last_name"],
+				 $params["email"],
+				$params["birthdate"],
+				 $params["phone"],
+				 $params["password"]);
+			$db = new DatabaseConnector("localhost", "TorrentMStreaming", "mysql", "", "root", "");
+			$conn = $db->getConnection();
 
-		//	http://localhost:81/TorrentMStreaming/user/?first_name=Mateus&last_name=Mourao&
-		//birthdate=1991-09-20&email=aladornxd@gmail.com&phone=81675250&password=restDOcAo123
 
-		return $conn->query($this->generateInsertQuery($user));
+	    return $conn->query($this->generateInsertQuery($user));
+	}else{
+		echo "Error 400: Bad Request";
+	}
 	}
 	private function generateInsertQuery($user)
 	{
-		$query =  "INSERT INTO user (first_name, last_name, email, birthdate, phone, pass) VALUES ('".$user->getName()."','".
-			$user->getLastName()."','".
-			$user->getEmail()."','".
-			$user->getBirthdate()."','".
-			$user->getPhone()."','".
-			$user->getPassword()."')";
+		$query = "INSERT INTO user (first_name, last_name, email, birthdate, phone, pass) VALUES ('" . $user->getName() . "','" .
+			$user->getLastName() . "','" .
+			$user->getEmail() . "','" .
+			$user->getBirthdate() . "','" .
+			$user->getPhone() . "','" .
+			$user->getPassword() . "')";
 		return $query;
 	}
+
+
 	public function search($request)
 	{
 		$params = $request->get_params();
@@ -51,25 +56,12 @@ class UserController
 		}
 		return substr($criteria, 0, -4);
 	}
-	private function validateParameters($parameters){
-		foreach ($this->requiredParameters as $key) {
-			if(!$this->isIn($key,$parameters)){
-				throw new Exception("Error! Parameter '$key' is missing in your request!", 1);
-			}
-		}
-	}
-	private function isIn($prm,$array){
-		foreach ($array as $key2 => $value) {
-			if ($key2 == $prm){
-				return true;
-			}
-		}
-		return false;
-	}
+//DELETE ----------------------------------
 	public function remove($request)
 	{
 		$params = $request->get_params();
-		$db = new DBConnector("localhost", "TorrentMStreaming", "mysql", "", "root", "");
+		$crit = $this->generateCriteria($params);
+		$db = new DatabaseConnector("localhost", "TorrentMStreaming", "mysql", "", "root", "");
 		$conn = $db->getConnection();
 		$result = $conn->prepare("DELETE FROM user WHERE first_name = ?");
 		$result->bindParam(1, $params['first_name']);
@@ -77,35 +69,40 @@ class UserController
 		return $result;
 	}
 
-
+// UPDATE-----------------------------------------------
 
 	public function update($request)
 	{
-		if(!empty($_GET["first_name"]) && !empty($_GET["last_name"]) && !empty($_GET["email"]) && !empty($_GET["birthdate"])&& !empty($_GET["phone"]) && !empty($_GET["password"]))
-		{
-			$first_name = addslashes(trim($_GET["first_name"]));
-			$last_name = addslashes(trim($_GET["last_name"]));
-			$email = addslashes(trim($_GET["email"]));
-			$birthdate = addslashes(trim($_GET["birthdate"]));
-			$phone = addslashes(trim($_GET["phone"]));
-			$password = addslashes(trim($_GET["password"]));
-			$params = $request->getParams();
-			$db = new DatabaseConnector("localhost", "TorrentMStreaming", "mysql", "", "root", "");
-			$conn = $db->getConnection();
-			$result = $conn->prepare("UPDATE user SET first_name=:first_name, last_name=:last_name, email=:email, birthdate=:birthdate, phone=:phone, password=:password WHERE email=:email");
-			$result->bindValue(":name", $first_name);
-			$result->bindValue(":cpf", $last_name);
-			$result->bindValue(":email", $email);
-			$result->bindValue(":birthdate", $birthdate);
-			$result->bindValue(":phone", $phone);
-			$result->bindValue(":password", $password);
-			$result->execute();
-
-			if ($result->rowCount() > 0){
-				echo "Usuário alterado com sucesso!";
-			} else {
-				echo "Usuário não atualizado";
-			}
-		}
+		$params = $request->get_params();
+		$db = new DatabaseConnector("localhost", "TorrentMStreaming", "mysql", "", "root", "");
+		$conn = $db->getConnection();
+		return $conn->query($this->generateUpdateQuery($params));
 	}
+	private function generateUpdateQuery($params)
+	{
+		$crit = $this->generateUpdateCriteria($params);
+		return "UPDATE user SET " . $crit . " WHERE id = '" . $params["id"] . "'";
+	}
+	private function generateUpdateCriteria($params)
+	{
+		$criteria = "";
+		foreach ($params as $key => $value)
+		{
+			$criteria = $criteria.$key." = '".$value."' ,";
+		}
+		return substr($criteria, 0, -2);
+	}
+
+//isvalid ------------
+
+	private function isValid($parameters)
+	{
+		$keys = array_keys($parameters);
+		$diff1 = array_diff($keys, $this->requiredParameters);
+		$diff2 = array_diff($this->requiredParameters, $keys);
+		if (empty($diff2) && empty($diff1))
+			return true;
+		return false;
+	}
+
 }
