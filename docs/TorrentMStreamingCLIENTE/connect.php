06eb754105a00edee11dab963d5c62327b935dda
Matthew <aladornxd@gmail.com>
2016-06-20 22:32:35 -0300

    Revert "não aceita video...editando/aprendendo "
    This reverts commit 205f9b92eacbb0a6ea74e5fcdc55044ec80b01c5.

--- docs/TorrentMStreamingCLIENTE/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once "DatabaseConnector.php";
-$db = new DatabaseConnector("localhost", "TorrentMStreaming", "mysql", "", "root", "");
-$conn = $db->getConnection();
-
-if (!$conn){
-    die("Database Connection Failed" . mysql_error());
-}
--- docs/TorrentMStreamingCLIENTE/get01.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Point to where you downloaded the phar
-include('httpful.phar');
-$url = "http://localhost:81/TorrentMStreaming/user/?first_name=".$_POST['first_name']."&last_name=".$_POST['last_name']."&email=".$_POST['email']."&birthdate=".$_POST['birthdate']."&phone=".$_POST['phone']."&password=".$_POST['password'];
-$response = \Httpful\Request::post($url)->send();
