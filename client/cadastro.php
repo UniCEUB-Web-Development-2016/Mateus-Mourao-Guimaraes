7d657bae3f1f0273c9b2b6092f12c76baf59dda7
Matthew <aladornxd@gmail.com>
2016-06-28 16:05:24 -0300

    Add files via upload

---
+++ client/cadastro.php
@@ -0,0 +1,13 @@
+<?php include("Cabecalho.php")?>
+    <center>
+        <table>
+            <form action="loginUsuario.php" method="post" name="form1">
+                <tr>
+                    <td>
+                        <h1> Cadastro Realizado com sucesso! </h1>
+                        <br>
+                    </td>
+                </tr>
+            </form>
+        </table>
+    </center>
